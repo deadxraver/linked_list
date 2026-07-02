ac7ffe2c8a4d015b380cfe5c8cf9483dd1347c71
deadxraver <deadxraver@yandex.ru>
2026-07-02 16:33:11 +0300

    examples

--- main.cpp
+++ main.cpp
@@ -8,6 +8,17 @@ int main(void) {
   std::cout << ll << std::endl;
   std::cout << ll[0] << ' ' << ll[1] << std::endl;
   std::cout << ll.size() << std::endl;
-  std::cout << ll[3] << std::endl;
+  ll.remove(42);
+  std::cout << ll << std::endl;
+  try {
+    ll.remove(42);
+  } catch (std::logic_error e) {
+    std::cerr << e.what() << std::endl;
+  }
+  try {
+    std::cout << ll[3] << std::endl;
+  } catch (std::logic_error e) {
+    std::cerr << e.what() << std::endl;
+  }
   return 0;
 }
