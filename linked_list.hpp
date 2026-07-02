2edc2de9ff75752af5bf008b10134348c13f1a15
deadxraver <deadxraver@yandex.ru>
2026-07-02 18:53:24 +0300

    improve readability

--- linked_list.hpp
+++ linked_list.hpp
@@ -15,39 +15,50 @@ private:
     T value;
     std::unique_ptr<list_node> next;
   };
+
   class linked_list_iterator {
   private:
     list_node* node_;
+
   public:
     using value_type = T;
     using reference = T&;
     using pointer = T*;
     using difference_type = std::ptrdiff_t;
     using iterator_category = std::forward_iterator_tag;
+
     explicit linked_list_iterator(list_node* node) : node_(node) {}
+
     reference operator*() {
       return node_->value;
     }
+
     pointer operator->() {
       return &node_->value;
     }
+
     linked_list_iterator& operator++() {
       node_ = node_->next.get();
       return *this;
     }
+
     linked_list_iterator operator++(int) {
       linked_list_iterator tmp(*this);
       ++(*this);
       return tmp;
     }
+
     bool operator==(const linked_list_iterator& other) const {
       return node_ == other.node_;
     }
+
     bool operator!=(const linked_list_iterator& other) const {
       return !(*this == other);
     }
   };
+
   std::unique_ptr<list_node> data_;
+
   void remove_next(list_node* node = nullptr) {
     if (node == nullptr) {
       if (this->data_.get() != nullptr)
@@ -61,6 +72,7 @@ private:
 
 public:
   using iterator = linked_list_iterator;
+
   linked_list_iterator begin() {
     return linked_list_iterator(this->data_.get());
   }
@@ -68,7 +80,9 @@ public:
   linked_list_iterator end() {
     return linked_list_iterator(nullptr);
   }
+
   explicit linked_list() : data_(nullptr) {}
+
   friend std::ostream& operator<<(std::ostream& os, const linked_list& ll) {
     os << "{";
     for (const list_node* p = ll.data_.get(); p != nullptr; p = p->next.get()) {
@@ -91,16 +105,27 @@ public:
       if (i == 0)
         return p->value;
     }
-    throw std::out_of_range("index " + std::to_string(idx) + " out of range for size " + std::to_string(size()));
+    throw std::out_of_range(
+        "index " + std::to_string(idx) +
+        " out of range for size " +
+        std::to_string(size())
+        );
   }
 
   const T& operator[](std::size_t i) const {
     std::size_t idx = i;
-    for (const list_node* p = this->data_.get(); p != nullptr; p = p->next.get(), --i) {
+    for (const list_node* p = this->data_.get();
+        p != nullptr;
+        p = p->next.get(), --i
+        ) {
       if (i == 0)
         return p->value;
     }
-    throw std::out_of_range("index " + std::to_string(idx) + " out of range for size " + std::to_string(size()));
+    throw std::out_of_range(
+        "index " + std::to_string(idx) +
+        " out of range for size " +
+        std::to_string(size())
+        );
   }
 
   void push(const T& value) {
@@ -137,19 +162,29 @@ public:
 
   void remove_at(std::size_t index) {
     std::size_t i = 0;
-    for (list_node* p = this->data_.get(), * prev = nullptr; p != nullptr; prev = p, p = p->next.get(), ++i) {
+    for (
+        list_node* p = this->data_.get(), * prev = nullptr;
+        p != nullptr;
+        prev = p, p = p->next.get(), ++i
+        ) {
       if (i == index) {
         remove_next(prev);
         return;
       }
     }
     throw std::out_of_range(
-        "index " + std::to_string(index) + " out of bounds for size " + std::to_string(this->size())
+        "index " + std::to_string(index) +
+        " out of bounds for size " +
+        std::to_string(this->size())
         );
   }
 
   void remove(const T& value) {
-    for (list_node* p = this->data_.get(), * prev = nullptr; p != nullptr; prev = p, p = p->next.get()) {
+    for (
+        list_node* p = this->data_.get(), * prev = nullptr;
+        p != nullptr;
+        prev = p, p = p->next.get()
+        ) {
       if (p->value == value) {
         remove_next(prev);
         return;
